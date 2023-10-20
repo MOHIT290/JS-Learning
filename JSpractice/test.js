84dc81f4c5110f9a515827f9ad4359d4516b0d66
MOHIT290 <mohitkaushik7737576582@gmail.com>
2023-10-20 17:22:16 +0000

    abc

--- JSpractice/test.js
+++ JSpractice/test.js
@@ -1,5 +1,24 @@
-console.log("Mohit!")
-
 const score="45t"
+const scorenum=Number(score)
 
-console.log(typeof score)
+// console.log(typeof score)
+// console.log("213">45)
+ //console.log(scorenum)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+/* 
+The reason is that an equality check == and comparisons  
+> < >= <= work differently. 
+Comparisons convert null to a number, treating it as 0.
+*/
+
+const arr=["Terminal","Leetcode"]
+
+console.log(typeof arr)
+
+
+
+
