2b65029dc0188c517bf3f95750801c9ebe63ef50
MOHIT290 <mohitkaushik7737576582@gmail.com>
2023-10-21 10:52:51 +0000

    abc

---
+++ JSpractice/num and maths.js
@@ -0,0 +1,22 @@
+const score=5000.78786576554
+const newnumber=new Number(5000)
+
+//toFixed will be used in ecommerce web application
+
+console.log(score.toFixed(2))
+
+const val=1000000056
+console.log(typeof val.toString())
+
+//++++++++++++++++++++Math++++++++++++++++++
+
+// console.log(Math.min((3,5,1)))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.2))
+
+console.log(Math.random()*10+1)
+
+const low=10,high=20
+
+console.log(Math.floor(Math.random()*(high-low))+low)
+
