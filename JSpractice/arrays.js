d1ffd688b7f9b6b7c92eeb2ef6c8b279572706f3
MOHIT290 <mohitkaushik7737576582@gmail.com>
2023-10-21 13:12:17 +0000

    abc

---
+++ JSpractice/arrays.js
@@ -0,0 +1,14 @@
+const arr=[1,2,3,4,5,6]
+
+const arr1=arr.slice(1,4)
+
+console.log("A ",arr)
+console.log("arr1 is ",arr1)
+
+const arr2=arr.splice(1,4)
+console.log("B ",arr)
+console.log("arr2 is ",arr2)
+
+const arr3=arr.splice(0,0)
+
+console.log("C ",arr)
