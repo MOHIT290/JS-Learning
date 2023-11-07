5c5509899619361985b93b110dc833d94d21bf18
MOHIT290 <mohitkaushik7737576582@gmail.com>
2023-11-07 10:36:42 +0000

    a

--- JSpractice/Async_Op/Promises.js
+++ JSpractice/Async_Op/Promises.js
@@ -1,11 +1,13 @@
-const promise1=new Promise(function(res,rej){
+const promise1 = new Promise(function (res, rej) {
 
-console.log("This is promise1, and it is getting executed")
-res()
+    setTimeout(function () {
+        console.log("This is promise1, and it is getting executed")
+        res()
+    }, 2000)
+
+  
 })
 
-promise1.then(function(){
-
+promise1.then(function () {
     console.log("Promise1 has been resolved")
-
 })
