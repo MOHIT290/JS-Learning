54503c1e8d5ab0e8ddfeb2f61526c8dd758c5445
MOHIT290 <mohitkaushik7737576582@gmail.com>
2023-11-07 15:38:57 +0000

    a

--- JSpractice/Async_Op/Promises.js
+++ JSpractice/Async_Op/Promises.js
@@ -1,13 +1,65 @@
+//Using then catch
+
 const promise1 = new Promise(function (res, rej) {
 
     setTimeout(function () {
-        console.log("This is promise1, and it is getting executed")
-        res()
+
+        const error = 0
+
+        if (!error) {
+            console.log("This is promise1, and it is getting executed")
+            res({ username: "Mohit", aim: "Paisa kamana" })
+        }
+
+        else {
+            rej('Error something went wrong')
+        }
+
     }, 2000)
 
-  
 })
 
-promise1.then(function () {
-    console.log("Promise1 has been resolved")
+promise1.then(function (name) {
+    return name.username
+}).then((name) => {
+    console.log(name)
+}).catch(function (err) {
+    console.log(err)
 })
+
+
+
+//Using try catch
+const promise2 = new Promise(function (res, rej) {
+
+    setTimeout(function () {
+
+        const error = 0
+
+        if (!error) {
+            console.log("This is promise1, and it is getting executed")
+            res({ username: "Mohit", aim: "Paisa kamana" })
+        }
+
+        else {
+            rej('Error something went wrong')
+        }
+
+    }, 2000)
+
+})
+
+async function consumepromise2() {
+
+    try {
+
+        const response = await promise2
+        console.log(response)
+
+    } catch (error) {
+
+    }
+
+}
+
+consumepromise2()
